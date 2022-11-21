b8f62adc49a79593c8ac80fd9453c4734dec54a4
Okute Wonah <okutewonah@gmail.com>
2022-11-21 09:09:42 +0200

    header logo

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../assets/images/logo.svg'
+
+const Header = () => {
+  return (
+    <header>
+      <img src={logo} alt='logo' />
+    </header>
+  )
+}
+
+export default Header

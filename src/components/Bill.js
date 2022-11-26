dc04b74cf66e4860740b14478d2e1e933deeae3b
Okute Wonah <okutewonah@gmail.com>
2022-11-26 07:07:10 +0200

    dollar sign in input

--- src/components/Bill.js
+++ src/components/Bill.js
@@ -4,6 +4,9 @@ const Bill = () => {
   return (
     <div className='bill-content'>
       <h1>Bill</h1>
+      <div className='bill-input'>
+        <input type='number' id='bill' className='bill' placeholder='0' />
+      </div>
     </div>
   )
 }

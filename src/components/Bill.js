86920b5062fd7b219333a437aa59f35e5afb9df3
Okute Wonah <okutewonah@gmail.com>
2022-11-27 08:05:05 +0200

    tip buttons creation

--- src/components/Bill.js
+++ src/components/Bill.js
@@ -7,6 +7,19 @@ const Bill = () => {
       <div className='bill-input'>
         <input type='number' id='bill' className='bill' placeholder='0' />
       </div>
+
+      {/* Tip Selection */}
+      <h2>Select Tip %</h2>
+      <div className='tips'>
+        <button>5%</button>
+        <button>10%</button>
+        <button>15%</button>
+        <button>25%</button>
+        <button>50%</button>
+        <input type='number' className='custom' placeholder='Custom' />
+      </div>
+      {/* Tip Selection */}
+
     </div>
   )
 }
